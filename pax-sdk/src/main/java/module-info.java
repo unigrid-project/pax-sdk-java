80f0dc1196292f16fc26a0987b00d16fcd395c19
dekm <gevan73@gmail.com>
2024-03-27 17:25:33 +0100

    exporting somemore modules required by fx

--- pax-sdk/src/main/java/module-info.java
+++ pax-sdk/src/main/java/module-info.java
@@ -19,8 +19,13 @@ module paxsdk {
 	exports org.unigrid.pax.sdk.cosmos.model;
 	exports cosmos.auth.v1beta1;
 	exports cosmos.bank.v1beta1;
+	exports cosmos.crypto.multisig;
+	exports cosmos.crypto.multisig.v1beta1;
+	exports cosmos.distribution.v1beta1;
 	exports cosmos.tx.v1beta1;
 	exports ibc.lightclients.tendermint.v1;
 	exports cosmos.staking.v1beta1;
 	exports cosmos.crypto.secp256k1;
+	exports gridnode.gridnode.v1;
+	exports cosmos.base.abci.v1beta1;
 }
