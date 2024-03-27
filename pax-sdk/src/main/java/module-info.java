1e79266d8a2e0ebd0f7288340d537cefe242ffb0
dekm <gevan73@gmail.com>
2024-03-27 17:32:34 +0100

    more exports that were missing

--- pax-sdk/src/main/java/module-info.java
+++ pax-sdk/src/main/java/module-info.java
@@ -17,15 +17,17 @@ module paxsdk {
 	// Exporting packages
 	exports org.unigrid.pax.sdk.cosmos;
 	exports org.unigrid.pax.sdk.cosmos.model;
+	exports gridnode.gridnode.v1;
 	exports cosmos.auth.v1beta1;
 	exports cosmos.bank.v1beta1;
+	exports cosmos.base.v1beta1;
 	exports cosmos.crypto.multisig;
 	exports cosmos.crypto.multisig.v1beta1;
 	exports cosmos.distribution.v1beta1;
 	exports cosmos.tx.v1beta1;
-	exports ibc.lightclients.tendermint.v1;
+	exports cosmos.base.abci.v1beta1;
+	exports cosmos.gov.v1beta1;
 	exports cosmos.staking.v1beta1;
 	exports cosmos.crypto.secp256k1;
-	exports gridnode.gridnode.v1;
-	exports cosmos.base.abci.v1beta1;
+	exports ibc.lightclients.tendermint.v1;	
 }
